650748c210f689ddcc070988c04380d99717ee8e
Slyyd <windows7kaue@gmail.com>
2025-03-03 10:29:00 -0300

    Criado as funções da classe SQLiteDatabaseHelpers

--- MauiMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiMinhasCompras.Models;
+using SQLite;
 
 namespace MauiMinhasCompras.Helpers
 {
-    internal class SQLiteDatabaseHelper
+    public class SQLiteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path) 
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>();
+        }
+
+        public Task<int> Insert(Produto prdt) 
+        {
+
+            return _conn.InsertAsync(prdt);
+
+        }
+
+        public Task<int> Update(Produto prdt) 
+        {
+
+            return _conn.UpdateAsync(prdt);
+
+        }
+
+        public Task<int> Delete(int id) 
+        {
+            return _conn.Table<Produto>().DeleteAsync(x => x.Id == id);
+        }
+
+        public Task<List<Produto>> ListAll() 
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> SearchTable(string query) 
+        {
+            string sql = "SELECT * Produto WHERE Descricao LIKE '?' ";
+
+            return _conn.QueryAsync<Produto>(sql, $"%{query}%");
+
+        }
+
     }
 }
